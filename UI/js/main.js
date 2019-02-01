d8383c27ffa3cf9484357b782958b7fc5d2aadf9
Paatrick Niyomwungeri <patriniyo12@gmail.com>
2019-02-01 14:17:34 -0800

    [Feature #163634021] finished both sign in and sign up pages

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,5 +1,9 @@
-const closeHomePageModal = document.getElementById("sign-in-modal-close");
-closeHomePageModal.addEventListener("click", ()=>{
+const closeSignUpModal = document.getElementById("sign-up-modal-close");
+closeSignUpModal.addEventListener("click", ()=>{
+	toggleModal("sign-up-modal");
+});
+const closeSignInModal = document.getElementById("sign-in-modal-close");
+closeSignInModal.addEventListener("click", ()=>{
 	toggleModal("sign-in-modal");
 });
 const signInButton = document.getElementById("sign-in-button");
@@ -8,7 +12,7 @@ signInButton.addEventListener("click", ()=>{
 });
 const signUpButton = document.getElementById("sign-up-button");
 signUpButton.addEventListener("click", ()=>{
-	toggleModal("sign-in-modal");
+	toggleModal("sign-up-modal");
 });
 const toggleModal = (id) =>{
 	let modal = document.getElementById(id);
