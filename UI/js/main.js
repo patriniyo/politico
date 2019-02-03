577c5361fbe96f6e548a7bb822683573a8305802
Paatrick Niyomwungeri <patriniyo12@gmail.com>
2019-02-03 13:58:36 -0800

    [#163680933] Add table of parties, delete and update buttons, update forms and confirmations popups

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,32 +1,109 @@
 const closeSignUpModal = document.getElementById("sign-up-modal-close");
-closeSignUpModal.addEventListener("click", ()=>{
-	toggleModal("sign-up-modal");
-});
-const closeSignInModal = document.getElementById("sign-in-modal-close");
-closeSignInModal.addEventListener("click", ()=>{
-	toggleModal("sign-in-modal");
-});
+const closeSignInModal = document.getElementById("form--sign-in");
 const signInButton = document.getElementById("sign-in-button");
-signInButton.addEventListener("click", ()=>{
-	toggleModal("sign-in-modal");
-});
 const signUpButton = document.getElementById("sign-up-button");
-signUpButton.addEventListener("click", ()=>{
-	toggleModal("sign-up-modal");
-});
-const toggleModal = (id) =>{
+const formSignIn = document.getElementById("form--sign-in");
+const formSignUp = document.getElementById("form--sign-up");
+const toggleModal = (id)=>{
 	let modal = document.getElementById(id);
-    modal.classList.toggle("show-modal");
+	modal.classList.toggle("show-modal");
+}
+if (closeSignUpModal) {
+	closeSignUpModal.addEventListener("click", ()=>{
+		toggleModal("sign-up-modal");
+	});
+}
+if(closeSignInModal) {
+	const closeSignInModal = document.getElementById("sign-in-modal-close");
+	closeSignInModal.addEventListener("click", ()=>{
+		toggleModal("sign-in-modal");
+	});
+}
+if (signInButton) {
+	signInButton.addEventListener("click", ()=>{
+		toggleModal("sign-in-modal");
+	});
+}
+if (signUpButton) {
+	signUpButton.addEventListener("click", ()=>{
+		toggleModal("sign-up-modal");
+	});
+}
+if (formSignIn) {
+	/*const formSignInSubmit = document.getElementById("form--sign-in-submit");*/
+	formSignIn.addEventListener("submit", (e)=>{
+		e.preventDefault();
+		window.location = "./ui/html/createparty.html";
+	});
+}
+if (formSignUp) {
+	formSignUp.addEventListener("submit", (e)=>{
+		e.preventDefault();
+		window.location = "../ui/html/createparty.html";
+	});
+}
+const buttonEdit = document.getElementsByClassName("button--edit");
+const buttonUpdate = document.getElementsByClassName("button--update");
+/*console.log(buttonEdit);*/
+if (buttonEdit) {
+	Array.from(buttonEdit).forEach((button)=>{
+		button.addEventListener("click",function(){
+			toggleModal("update-party-modal");
+		});
+	});
+}
+if (buttonUpdate) {
+	Array.from(buttonUpdate).forEach((button)=>{
+		button.addEventListener("click",function(){
+			toggleModal("delete-party-modal");
+		});
+	});
+}
+const deletePartyClose = document.getElementById("delete-party-close");
+if (deletePartyClose){
+	deletePartyClose.addEventListener("click",function(){
+		toggleModal("delete-party-modal");
+	});
+}
+const deletePartyCancel = document.getElementById("delete-party-modal-cancel");
+if (deletePartyCancel) {
+	deletePartyCancel.addEventListener("click",function(){
+		toggleModal("delete-party-modal");
+	});
+}
+const deletePartyOk = document.getElementById("delete-party-modal-ok");
+if (deletePartyOk) {
+	deletePartyOk.addEventListener("click",function(){
+		toggleModal("delete-party-modal");
+	});
+}
+const updatePartyClose = document.getElementById("update-party-modal-close");
+if (updatePartyClose) {
+	updatePartyClose.addEventListener("click",function(){
+		toggleModal("update-party-modal");
+	});
+}
+const updatePartySubmit = document.getElementById("update-party-submit");
+if (updatePartySubmit) {
+	updatePartySubmit.addEventListener("click",function(e){
+		e.preventDefault();
+		toggleModal("update-party-confirmation-modal");
+		toggleModal("update-party-modal");
+	});
 }
 
-/*const formSignInSubmit = document.getElementById("form--sign-in-submit");*/
-const formSignIn = document.getElementById("form--sign-in");
-formSignIn.addEventListener("submit", (e)=>{
-	e.preventDefault();
-	window.location = "./ui/html/createparty.html";
-});
-const formSignUp = document.getElementById("form--sign-up");
-formSignUp.addEventListener("submit", (e)=>{
-	e.preventDefault();
-	window.location = "../ui/html/createparty.html";
-});
+const updatePartyConfirmationClose = document.getElementById("update-party-confirmation-close");
+if (updatePartyConfirmationClose) {
+	updatePartyConfirmationClose.addEventListener("click",function(){
+		toggleModal("update-party-confirmation-modal");
+	});
+}
+const updatePartyConfirmationOk = document.getElementById("update-party-confirmation-ok");
+if (updatePartyConfirmationOk) {
+	updatePartyConfirmationOk.addEventListener("click",function(){
+		toggleModal("update-party-confirmation-modal");
+	});
+}
+
+
+
