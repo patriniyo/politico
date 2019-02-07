4f80c5be78cb27689460540251cec682cd9001b5
Paatrick Niyomwungeri <patriniyo12@gmail.com>
2019-02-07 13:08:03 -0800

    [Feature#163785895] creating create party endpoint

---
+++ server/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import Party from './controllers/party';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/api/v1/parties', Party.createParty);
+
+/* app.use('/api/v1/parties', parties); */
+
+/* app.use((req, res) => {
+  res.status(404).send({
+    status: 404,
+    error: 'Resources not found'
+  });
+}); */
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.info(`the server has started on ${PORT}`));
+
+export default app;
